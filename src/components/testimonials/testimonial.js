e6bc5c0c36f1a4daa8cbadead3279897d67ce4be
armioz <phathai.s50@gmail.com>
2019-11-08 14:36:30 +0700

    initial react x

---
+++ src/components/testimonials/testimonial.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+export default class testimonial extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      {/*generated code*/}
+      <header id="home">
+        ....
+      </header>
+      </React.Fragment>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,83 @@
+let resumeData = {
+  "imagebaseurl":"https://drive.google.com/open?id=11MMxZZ9sJNfylPW0cqMo5l2Bbd5VnNop",
+  "name": "Phathai Sangsirirojh",
+  "role": "Teacher,Programmer",
+  "linkedinId":" phathai sangsirirojh",
+  "roleDescription": "I  like to learn about new technologies, networking, Programming , Photography or simply play games in my free time.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/phathai-sangsirirojh-07a408187/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"facebook",
+        "url":"https://www.facebook.com/phathai.sangsirirojh.5",
+        "className":"fa fa-facebook"
+      },
+      {
+        "name":"facebook",
+        "url":"https://www.flickr.com/photos/155530526@N03/",
+        "className":"fa fa-flickr"
+      },
+      {
+        "name":"facebook",
+        "url":"mailto:phathai.s50@gmail.com",
+        
+        "className":"fa fa-envelope",
+        
+      }
+  ],
+  "aboutme":"I have finished in Computer Education (Computer Engineering) and have been working in teacher roles for seven years.",
+  "address":"319/119 m.4 nongjom sansai Chiangmai",
+  "website":"#",
+  "education":[
+    {
+      "UniversityName":"Payap University",
+      "Major":"Master of Business Administration",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2017",
+      "Achievements":"Management,Logistics,Research,Marketing"
+    },
+    {
+      "UniversityName":"rajamangala university of technology lanna",
+      "Major":"Computer Education (Computer Engineering)",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2012",
+      "Achievements":"JAVA,C#,SQL,Network,Basic Router and Switch Setup, Static Route, RIP, OSPF, VLAN, Packet Tracer,Linux "
+    },
+    {
+      "UniversityName":"rajamangala university of technology lanna",
+      "Major":"Teachnic Computer",
+      "MonthOfPassing":"may",
+      "YearOfPassing":"2008",
+      "Achievements":"Computer Maintenance,C Programming"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Chiangmai Technical Colleage",
+      "Major":"Teach in computer roles",
+      "MonthOfLeaving":"Oct",
+      "YearOfLeaving":"2019",
+      "Achievements":"Some Achievements"
+    }],
+  "skillsDescription":"Computer Maintenance,C Programming,JAVA,C#,SQL,Network,Basic Router and Switch Setup, Static Route, RIP, OSPF, VLAN, Packet Tracer,Linux ",
+  
+  "portfolio":[
+    {
+      "name":"Lung machine exercise",
+      "description":"เครื่องบริหารปอด",
+      "url":"https://www.flickr.com/photos/155530526@N03/",
+      "imgurl":"images/lung.jpg"
+    },
+    {
+      "name":"ระบบบริหารข้อมูลสำหรับฟาร์มสุนัข",
+      "description":"ระบบควบคุมไฟฟ้าในศูนย์ฝึกและรับเลี้ยงสุนัข Rasberries Pi",
+      "url":"https://www.flickr.com/photos/155530526@N03/",
+      "imgurl":"images/sys.jpg"
+    }
+  ]
+}
+
+export default resumeData
